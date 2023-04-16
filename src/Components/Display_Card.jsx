155571a93d552fd45f114027f311355d1d3c7643
sidhantkaushal <kaushalsidhant434@gmail.com>
2023-04-16 18:49:12 +0530

    added img

--- src/Components/Display_Card.jsx
+++ src/Components/Display_Card.jsx
@@ -11,7 +11,7 @@ function Display_Card({ img, country, loc, locLink, time, desc }) {
         <div className="place-desc">
           <img
             className="location-icon"
-            src="public/Images/location.png"
+            src="https://freepngimg.com/download/symbol/69580-symbol-computer-location-icons-hq-image-free-png.png"
             alt=""
           />
           <span className="Countryname">{country}</span>
